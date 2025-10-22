e23f6802231f7812eef03d5fe62731cb571a85ba
itsmeumaima <umaimarauf2005@gmail.com>
2025-10-22 21:53:51 +0500

    searching half done

--- postsync/item/views.py
+++ postsync/item/views.py
@@ -1,10 +1,16 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,get_object_or_404, redirect
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 # Create your views here.
+def items(request):
+    items=Item.objects.filter(is_sold=False)
+    return render(request, 'item/items.html',{
+        'items':items
+    })
+
 def detail(request, pk):
     item=get_object_or_404(Item, pk=pk)
     related_items=Item.objects.filter(category=item.category,is_sold=False).exclude(pk=pk)[0:3]
@@ -39,3 +45,21 @@ def delete(request,pk):
 
     item.delete()
     return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('item:detail', pk=item.id)
+    else:
+        form = EditItemForm(instance=item)
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit item',
+    })
