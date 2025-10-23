82e496dc41e4165db24ccf0103172e9bd983e489
itsmeumaima <umaimarauf2005@gmail.com>
2025-10-23 11:22:49 +0500

    searching completed

--- postsync/item/views.py
+++ postsync/item/views.py
@@ -1,14 +1,29 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,get_object_or_404, redirect
+from django.db.models import Q
 
 from .forms import NewItemForm, EditItemForm
-from .models import Item
+from .models import Item, Category
+
 
 # Create your views here.
 def items(request):
+    query=request.GET.get('query','')
     items=Item.objects.filter(is_sold=False)
+    categories=Category.objects.all()
+    category_id = request.GET.get('category',0)
+    
+    if category_id:
+        items=items.filter(category_id=category_id)
+    if query:
+        items=items.filter(Q(name__icontains=query) | Q(description__icontains=query))
+    
     return render(request, 'item/items.html',{
-        'items':items
+        'items':items,
+        'query':query,
+        'categories':categories,
+        'category_id':int(category_id),
+
     })
 
 def detail(request, pk):
